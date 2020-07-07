740d8e1189935adbe263bd58eaa697f4a36cba61
Ross Cameron <rosscameron93@gmail.com>
2020-07-07 17:01:41 +0100

    updated aspect components

--- src/Aspect.cpp
+++ src/Aspect.cpp
@@ -116,43 +116,25 @@ struct AspectWidget : ModuleWidget
         setModule(module);
         setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Aspect.svg")));
 
-        addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-        addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(createWidget<FFHexScrew>(Vec(RACK_GRID_WIDTH, 0)));
+        addChild(createWidget<FFHexScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+        addChild(createWidget<FFHexScrew>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+        addChild(createWidget<FFHexScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-        addInput(createInputCentered<PJ301MPort>(mm2px(Vec(11.324, 9.143)), module, Aspect::TRIG_INPUT));
-        addInput(createInputCentered<PJ301MPort>(mm2px(Vec(11.324, 25.165)), module, Aspect::RESET_INPUT));
+        addInput(createInputCentered<FF01JKPort>(mm2px(Vec(18.714, 23.417)), module, Aspect::TRIG_INPUT));
+        addInput(createInputCentered<FF01JKPort>(mm2px(Vec(18.714, 36.251)), module, Aspect::RESET_INPUT));
 
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(11.324, 41.188)), module, Aspect::D2_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(11.324, 57.21)), module, Aspect::D4_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(11.324, 73.232)), module, Aspect::D8_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(11.324, 89.255)), module, Aspect::D16_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(11.324, 105.277)), module, Aspect::D32_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(11.324, 121.299)), module, Aspect::D64_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(32.209, 9.753)), module, Aspect::S1_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(32.209, 25.775)), module, Aspect::S2_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(32.209, 41.797)), module, Aspect::S3_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(32.209, 57.82)), module, Aspect::S4_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(32.209, 73.842)), module, Aspect::S5_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(32.209, 89.864)), module, Aspect::S6_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(32.209, 105.886)), module, Aspect::S7_OUTPUT));
-        addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(32.209, 121.909)), module, Aspect::S8_OUTPUT));
+        for (int i = 0; i < 6; i++)
+        {
+            addOutput(createOutputCentered<FF01JKPort>(mm2px(Vec(18.714, 49.09 + i * 12.83)), module, Aspect::D2_OUTPUT + i));
+            addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(25.714, 49.09 + i * 12.83)), module, Aspect::D2_LIGHT + i));
+        }
 
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(3.048, 41.188)), module, Aspect::D2_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(3.048, 57.21)), module, Aspect::D4_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(3.048, 73.232)), module, Aspect::D8_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(3.048, 89.255)), module, Aspect::D16_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(3.048, 105.277)), module, Aspect::D32_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(3.048, 121.299)), module, Aspect::D64_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.933, 9.55)), module, Aspect::S1_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.933, 25.601)), module, Aspect::S2_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.933, 41.797)), module, Aspect::S3_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.933, 57.82)), module, Aspect::S4_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.933, 73.842)), module, Aspect::S5_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.933, 89.864)), module, Aspect::S6_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.933, 105.886)), module, Aspect::S7_LIGHT));
-        addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(23.933, 121.909)), module, Aspect::S8_LIGHT));
+        for (int i = 0; i < 8; i++)
+        {
+            addOutput(createOutputCentered<FF01JKPort>(mm2px(Vec(38.771, 23.417 + i * 12.83)), module, Aspect::S1_OUTPUT + i));
+            addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(45.771, 23.417 + i * 12.83)), module, Aspect::S1_LIGHT + i));
+        }
     }
 };
 
