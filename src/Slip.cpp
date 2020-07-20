b2358e4eb653510638f61a9918ae7d2e43dfff33
Ross Cameron <rosscameron93@gmail.com>
2020-07-20 21:02:15 +0100

    most functionality added to slip, still need to add delay

--- src/Slip.cpp
+++ src/Slip.cpp
@@ -1,27 +1,67 @@
 #include "plugin.hpp"
 
+struct SlewLimiter
+{
+	// Taken from Befaco Slew Limiter
+	// https://github.com/VCVRack/Befaco/blob/v1/src/SlewLimiter.cpp
+
+	float out = 0.0;
+
+	float process(float input, float amount, float delta)
+	{
+		float shape = 0.f;
+
+		// minimum and maximum slopes in volts per second
+		const float slewMin = 0.1f;
+		const float slewMax = 10000.f;
+		// Amount of extra slew per voltage difference
+		const float shapeScale = 1 / 10.f;
+
+		// Rise
+		if (input > out)
+		{
+			float rise = amount;
+			float slew = slewMax * std::pow(slewMin / slewMax, rise);
+			out += slew * crossfade(1.f, shapeScale * (input - out), shape) * delta;
+			if (out > input)
+				out = input;
+		}
+		// Fall
+		else if (input < out)
+		{
+			float fall = amount;
+			float slew = slewMax * std::pow(slewMin / slewMax, fall);
+			out -= slew * crossfade(1.f, shapeScale * (out - input), shape) * delta;
+			if (out < input)
+				out = input;
+		}
+
+		return out;
+	}
+};
+
 struct Slip : Module
 {
 	enum ParamIds
 	{
 		THRESH_PARAM,
 		WET_PARAM,
-		HIGHSHIFTTRIM_PARAM,
-		HIGHPINCHTRIM_PARAM,
-		HIGHFOLDTRIM_PARAM,
-		HIGHSLEWTRIM_PARAM,
-		HIGHSHIFT_PARAM,
-		HIGHPINCH_PARAM,
-		HIGHFOLD_PARAM,
-		HIGHSLEW_PARAM,
-		LOWSHIFT_PARAM,
-		LOWPINCH_PARAM,
-		LOWFOLD_PARAM,
-		LOWSLEW_PARAM,
 		LOWSHIFTTRIM_PARAM,
+		HIGHSHIFTTRIM_PARAM,
 		LOWPINCHTRIM_PARAM,
+		HIGHPINCHTRIM_PARAM,
 		LOWFOLDTRIM_PARAM,
+		HIGHFOLDTRIM_PARAM,
 		LOWSLEWTRIM_PARAM,
+		HIGHSLEWTRIM_PARAM,
+		LOWSHIFT_PARAM,
+		HIGHSHIFT_PARAM,
+		LOWPINCH_PARAM,
+		HIGHPINCH_PARAM,
+		LOWFOLD_PARAM,
+		HIGHFOLD_PARAM,
+		LOWSLEW_PARAM,
+		HIGHSLEW_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds
@@ -29,14 +69,14 @@ struct Slip : Module
 		THRESH_INPUT,
 		IN_INPUT,
 		WET_INPUT,
-		HIGHSHIFT_INPUT,
-		HIGHPINCH_INPUT,
-		HIGHFOLD_INPUT,
-		HIGHSLEW_INPUT,
 		LOWSHIFT_INPUT,
+		HIGHSHIFT_INPUT,
 		LOWPINCH_INPUT,
+		HIGHPINCH_INPUT,
 		LOWFOLD_INPUT,
+		HIGHFOLD_INPUT,
 		LOWSLEW_INPUT,
+		HIGHSLEW_INPUT,
 		NUM_INPUTS
 	};
 	enum OutputIds
@@ -54,28 +94,118 @@ struct Slip : Module
 	Slip()
 	{
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(THRESH_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(THRESH_PARAM, -10.f, 10.f, 0.f, "");
 		configParam(WET_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(HIGHSHIFTTRIM_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(HIGHPINCHTRIM_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(HIGHFOLDTRIM_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(HIGHSLEWTRIM_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(HIGHSHIFT_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(HIGHPINCH_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(HIGHFOLD_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(HIGHSLEW_PARAM, 0.f, 1.f, 0.f, "");
+
+		for (int i = 0; i < 8; ++i)
+		{
+			configParam(LOWSHIFTTRIM_PARAM + i, -1.f, 1.f, 0.f, "");
+		}
+
 		configParam(LOWSHIFT_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(LOWPINCH_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(LOWFOLD_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(HIGHSHIFT_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(LOWPINCH_PARAM, -1.f, 1.f, 0.f, "");
+		configParam(HIGHPINCH_PARAM, -1.f, 1.f, 0.f, "");
+		configParam(LOWFOLD_PARAM, 0.1f, 1.f, 1.f, "");
+		configParam(HIGHFOLD_PARAM, 0.1f, 1.f, 1.f, "");
 		configParam(LOWSLEW_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(LOWSHIFTTRIM_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(LOWPINCHTRIM_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(LOWFOLDTRIM_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(LOWSLEWTRIM_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(HIGHSLEW_PARAM, 0.f, 1.f, 0.f, "");
+	}
+
+	SlewLimiter slewLims[2];
+
+	float pincher(float input, float amount)
+	{
+		// Taken from HetrickCV Waveshaper
+		// https://github.com/mhetrick/hetrickcv/blob/master/src/Waveshape.cpp
+		input *= 0.2;
+
+		amount *= 0.99f;
+		const float shapeB = (1.0 - amount) / (1.0 + amount);
+		const float shapeA = (4.0 * amount) / ((1.0 - amount) * (1.0 + amount));
+
+		float output = input * (shapeA + shapeB);
+		output = output / ((std::abs(input) * shapeA) + shapeB);
+
+		return output * 5;
+	}
+
+	float folder(float input, float amount)
+	{
+		// Taken from Autodafe FoldBack
+		// https://github.com/antoniograzioli/Autodafe/blob/master/src/FoldBack.cpp
+		input = input * 0.1f;
+		if (input > amount || input < -amount)
+		{
+			input = fabs(fabs(fmod(input - amount, amount * 4)) - amount * 2) - amount;
+		}
+		return input * 10.f;
 	}
 
 	void process(const ProcessArgs &args) override
 	{
+		// Get input sample
+		float input = inputs[IN_INPUT].getVoltage();
+
+		// Split Input into high and low
+		// What constitutes high and low is determined by the threshold
+		float threshold = params[THRESH_PARAM].getValue();
+		threshold += inputs[THRESH_INPUT].getVoltage();
+		threshold = clamp(threshold, -10.f, 10.f);
+
+		// Is the current sample above or below the threshold?
+		bool high = input > threshold;
+		// int high = 0;
+
+		// Get parameters
+		// The sample will run through the same type of process if it's high or low
+		// The difference is which paramaters to use for high or low processing
+
+		// We've interleaved the parameter ENUMS so we can avoid an if branch by simply adding the high state to the index
+		float shift = params[LOWSHIFT_PARAM + high].getValue();
+		shift += inputs[LOWSHIFT_INPUT + high].getVoltage() * 0.1f * params[LOWSHIFTTRIM_PARAM + high].getValue();
+		shift = clamp(shift, 0.f, 1.f);
+
+		float pinch = params[LOWPINCH_PARAM + high].getValue();
+		pinch += inputs[LOWPINCH_INPUT + high].getVoltage() * params[LOWPINCHTRIM_PARAM + high].getValue();
+		pinch = clamp(pinch, -1.f, 1.f);
+
+		float fold = params[LOWFOLD_PARAM + high].getValue();
+		fold += inputs[LOWFOLD_INPUT + high].getVoltage() * 0.1f * params[LOWFOLDTRIM_PARAM + high].getValue();
+		fold = clamp(fold, 0.1f, 1.f);
+
+		float slew = params[LOWSLEW_PARAM + high].getValue();
+		slew += inputs[LOWSLEW_INPUT + high].getVoltage() * 0.1f * params[LOWSLEWTRIM_PARAM + high].getValue();
+		slew = clamp(slew, 0.f, 1.f);
+
+		// Processing
+		// shift - taken from vcv delay
+
+		// pinch - taken from HetrickCV Waveshaper
+		input = pincher(input, pinch);
+
+		// fold - taken from Autodafe wavefolder
+		input = folder(input, fold);
+
+		// slew - taken from befaco slew limiter
+		input = slewLims[high].process(input, slew, args.sampleTime);
+
+		// Output high and low components
+		if (high)
+		{
+			outputs[HIGH_OUTPUT].setVoltage(input);
+			outputs[LOW_OUTPUT].setVoltage(threshold);
+			// outputs[LOW_OUTPUT].setVoltage(0);	// This gives a great weird pulsey "gapped" wave
+		}
+		else
+		{
+			outputs[HIGH_OUTPUT].setVoltage(threshold);
+			// outputs[HIGHA_OUTPUT].setVoltage(0);	// This gives a great weird pulsey "gapped" wave
+			outputs[LOW_OUTPUT].setVoltage(input);
+		}
+
+		// Output main
+		outputs[OUT_OUTPUT].setVoltage(input);
 	}
 };
 
@@ -94,36 +224,36 @@ struct SlipWidget : ModuleWidget
 		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(23.097, 23.404)), module, Slip::THRESH_PARAM));
 		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(78.503, 23.404)), module, Slip::WET_PARAM));
 
+		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(17.82, 90.023)), module, Slip::LOWSHIFTTRIM_PARAM));
 		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(17.82, 48.282)), module, Slip::HIGHSHIFTTRIM_PARAM));
+		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(38.813, 90.023)), module, Slip::LOWPINCHTRIM_PARAM));
 		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(38.813, 48.282)), module, Slip::HIGHPINCHTRIM_PARAM));
+		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(62.807, 90.023)), module, Slip::LOWFOLDTRIM_PARAM));
 		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(62.807, 48.282)), module, Slip::HIGHFOLDTRIM_PARAM));
+		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(83.8, 90.023)), module, Slip::LOWSLEWTRIM_PARAM));
 		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(83.8, 48.282)), module, Slip::HIGHSLEWTRIM_PARAM));
 
-		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(20.82, 59.027)), module, Slip::HIGHSHIFT_PARAM));
-		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(40.813, 59.027)), module, Slip::HIGHPINCH_PARAM));
-		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(60.807, 59.027)), module, Slip::HIGHFOLD_PARAM));
-		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(80.8, 59.027)), module, Slip::HIGHSLEW_PARAM));
 		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(20.82, 77.584)), module, Slip::LOWSHIFT_PARAM));
+		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(20.82, 59.027)), module, Slip::HIGHSHIFT_PARAM));
 		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(40.813, 77.584)), module, Slip::LOWPINCH_PARAM));
+		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(40.813, 59.027)), module, Slip::HIGHPINCH_PARAM));
 		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(60.807, 77.584)), module, Slip::LOWFOLD_PARAM));
+		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(60.807, 59.027)), module, Slip::HIGHFOLD_PARAM));
 		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(80.8, 77.584)), module, Slip::LOWSLEW_PARAM));
-
-		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(17.82, 90.023)), module, Slip::LOWSHIFTTRIM_PARAM));
-		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(38.813, 90.023)), module, Slip::LOWPINCHTRIM_PARAM));
-		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(62.807, 90.023)), module, Slip::LOWFOLDTRIM_PARAM));
-		addParam(createParamCentered<FF06GKnob>(mm2px(Vec(83.8, 90.023)), module, Slip::LOWSLEWTRIM_PARAM));
+		addParam(createParamCentered<FF10GKnob>(mm2px(Vec(80.8, 59.027)), module, Slip::HIGHSLEW_PARAM));
 
 		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(8.0, 23.417)), module, Slip::THRESH_INPUT));
 		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(50.758, 23.417)), module, Slip::IN_INPUT));
 		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(93.6, 23.417)), module, Slip::WET_INPUT));
-		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(14.82, 36.251)), module, Slip::HIGHSHIFT_INPUT));
-		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(36.813, 36.251)), module, Slip::HIGHPINCH_INPUT));
-		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(64.807, 36.251)), module, Slip::HIGHFOLD_INPUT));
-		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(86.8, 36.251)), module, Slip::HIGHSLEW_INPUT));
+
 		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(14.82, 100.386)), module, Slip::LOWSHIFT_INPUT));
+		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(14.82, 36.251)), module, Slip::HIGHSHIFT_INPUT));
 		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(36.813, 100.386)), module, Slip::LOWPINCH_INPUT));
+		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(36.813, 36.251)), module, Slip::HIGHPINCH_INPUT));
 		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(64.807, 100.386)), module, Slip::LOWFOLD_INPUT));
+		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(64.807, 36.251)), module, Slip::HIGHFOLD_INPUT));
 		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(86.8, 100.386)), module, Slip::LOWSLEW_INPUT));
+		addInput(createInputCentered<FF01JKPort>(mm2px(Vec(86.8, 36.251)), module, Slip::HIGHSLEW_INPUT));
 
 		addOutput(createOutputCentered<FF01JKPort>(mm2px(Vec(26.0, 113.225)), module, Slip::LOW_OUTPUT));
 		addOutput(createOutputCentered<FF01JKPort>(mm2px(Vec(50.8, 113.225)), module, Slip::OUT_OUTPUT));
