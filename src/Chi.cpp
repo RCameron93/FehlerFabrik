797ee45117b6c3b5ded05aee0ba58f30ca5e1ce5
Ross Cameron <rosscameron93@gmail.com>
2020-08-16 19:35:47 +0100

    working OK in mono

--- src/Chi.cpp
+++ src/Chi.cpp
@@ -7,95 +7,6 @@
 
 #include "plugin.hpp"
 
-// Magic numbers for the isolater band frequency ranges
-static const float 	FREQ_E2 = 82.40689f;
-static const float 	FREQ_E5 = 659.2551f;
-static const float 	FREQ_C6 = 1046.502f;
-static const float 	FREQ_C9 = 8372.018f;
-
-struct LinkwitzRileyFilter {
-	// Second order
-	// Pirkle - Designing Audio Effect Plugins in C++
-	// Obviously we'll be tidying this one up at some point
-
-	float omega;
-	float theta;
-	float kappa;
-	float delta;
-
-	float xStateLP[3] = {0.f};
-	float yStateLP[3] = {0.f};
-
-	float xCoeffLP[3] = {0.f};
-	
-	// Y coefficients are shared
-	float yCoeff[2] = {0.f};
-
-	float xStateHP[3] = {0.f};
-	float yStateHP[3] = {0.f};
-
-	float xCoeffHP[3] = {0.f};
-
-	float outLP = 0.f;
-	float outHP = 0.f;
-
-
-	void setCutoff(float fc, float fs)
-	{
-		// fc = cutoff frequency, fs = sampling frequency
-		theta = M_PI * fc / fs;
-		omega = M_PI * fc;
-		kappa = omega/tan(theta);
-		delta = kappa * kappa + omega * omega + 2 * kappa * omega;
-	}
-
-	void setLPFCoeffs()
-	{
-		xCoeffLP[0] = omega * omega / delta;
-		xCoeffLP[1] = 2 * omega * omega / delta;
-		xCoeffLP[2] = omega * omega / delta;
-	}
-
-	void setHPFCoeffs()
-	{
-		xCoeffHP[0] = kappa * kappa / delta;
-		xCoeffHP[1] = -2 * kappa * kappa / delta;
-		xCoeffHP[2] = kappa * kappa / delta;
-	}
-
-	void setYCoeffs()
-	{
-		yCoeff[0] = (-2 * kappa * kappa + 2 * omega * omega) / delta;
-		yCoeff[1] = (-2 * kappa * omega + kappa * kappa + omega * omega) / delta;
-	}
-
-
-	void process(float input)
-	{
-		xStateLP[0] = input;
-		yStateLP[0] = xCoeffLP[0] * xStateLP[0] + xCoeffLP[1] * xStateLP[1] + xCoeffLP[2] * xStateLP[2] - yCoeff[0] * yStateLP[1] - yCoeff[1] * yStateLP[2];
-		
-		xStateLP[2] = xStateLP[1];
-		xStateLP[1] = xStateLP[0];
-
-		yStateLP[2] = yStateLP[1];
-		yStateLP[1] = yStateLP[0];
-
-		outLP = yStateLP[0];
-
-		xStateHP[0] = input;
-		yStateHP[0] = xCoeffHP[0] * xStateHP[0] + xCoeffHP[1] * xStateHP[1] + xCoeffHP[2] * xStateHP[2] - yCoeff[0] * yStateHP[1] - yCoeff[1] * yStateHP[2];
-		
-		xStateHP[2] = xStateHP[1];
-		xStateHP[1] = xStateHP[0];
-
-		yStateHP[2] = yStateHP[1];
-		yStateHP[1] = yStateHP[0];
-
-		outHP = yStateHP[0];
-	}
-};
-
 struct ButterWorthFilter {
 	// Second order
 	// Pirkle - Designing Audio Effect Plugins in C++
@@ -246,12 +157,12 @@ struct Chi : Module
 	Chi()
 	{
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(LOW_GAIN_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(MID_GAIN_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(HIGH_GAIN_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(LOW_GAIN_CV_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(MID_GAIN_CV_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(HIGH_GAIN_CV_PARAM, 0.f, 1.f, 0.f, "");
+		configParam(LOW_GAIN_PARAM, 0.f, 2.f, 1.f, "Low Gain");
+		configParam(MID_GAIN_PARAM, 0.f, 2.f, 1.f, "Mid Gain");
+		configParam(HIGH_GAIN_PARAM, 0.f, 2.f, 1.f, "High Gain");
+		configParam(LOW_GAIN_CV_PARAM, -1.f, 1.f, 0.f, "Low Gain CV Trim");
+		configParam(MID_GAIN_CV_PARAM, -1.f, 1.f, 0.f, "Mid Gain CV Trim");
+		configParam(HIGH_GAIN_CV_PARAM, -1.f, 1.f, 0.f, "High Gain CV Trim");
 		configParam(LOW_X_PARAM, 0.f, 1.f, 0.5f, "Low/Mid X Freq");
 		configParam(HIGH_X_PARAM, 0.f, 1.f, 0.5f, "Mid/High X Freq");
 	}
@@ -260,55 +171,75 @@ struct Chi : Module
 	float lowFreqScale (float knobValue)
 	{
 		// Converts a knob value from 0 -> 0.5 -> 1 to 80 -> 225 -> 640
-		float scaled = 540 * knobValue * knobValue + 20 * knobValue + 80;
-		return scaled;
+		// float scaled = 540 * knobValue * knobValue + 20 * knobValue + 80;
+		return 600;
 	}
 
 	float highFreqScale (float knobValue)
 	{
 		// Converts a knob value from 0 -> 0.5 -> 1 to 1k -> 2.8k -> 8k
-		float scaled = 6800 * knobValue * knobValue + 200 * knobValue + 1000;
-		return scaled;
+		// float scaled = 6800 * knobValue * knobValue + 200 * knobValue + 1000;
+		return 2800;
 	}
 
 	void process(const ProcessArgs &args) override
 	{
 		float in = inputs[IN_INPUT].getVoltage();
 
-		// Get parameters
+		/////////////// Get parameters
+
+		// Frequencies
 		// For now we're just taking MP2015 frequency ranges
 		// Start with all values [0:1]
 		float lowX = params[LOW_X_PARAM].getValue();
 		lowX += inputs[LOW_X_INPUT].getVoltage() / 10.f;
-		lowX = clamp(lowX, -1.f, 1.f);
+		lowX = clamp(lowX, 0.f, 1.f);
 		// Convert to Hz
 		lowX = lowFreqScale(lowX);
 
 		// Start with all values [0:1]
 		float highX = params[HIGH_X_PARAM].getValue();
 		highX += inputs[HIGH_X_INPUT].getVoltage() / 10.f;
-		highX = clamp(highX, -1.f, 1.f);
+		highX = clamp(highX, 0.f, 1.f);
 		// Convert to Hz
 		highX = highFreqScale(highX);
 
+		// Gains
+		// We go up to 2.0 because we want the right hand side of the knob to be 0dB -> +6dB
+
+		float gains[3] = {0.f};
+		for (int i = 0; i < 3; ++i)
+		{
+			gains[i] = params[LOW_GAIN_PARAM + i].getValue();
+			gains[i] += inputs[LOW_GAIN_INPUT + i].getVoltage() * params[LOW_GAIN_CV_PARAM + i].getValue();
+			gains[i] = clamp(gains[i], 0.f, 2.f);
+		}
+
+		/////////////// Process
+
+		float outs[3] = {0.f};
+
 		// Process low/mid Xover
 		filter[0].setCoefficients(lowX, args.sampleRate);
-		float lowOut = filter[0].lowpass(in);
-		float midOut = filter[0].highpass(in);
+		outs[0] = filter[0].lowpass(in);
+		outs[1] = filter[0].highpass(in);
 
 		// Process mid/high Xover
 		filter[1].setCoefficients(highX, args.sampleRate);
-		float highOut = filter[1].highpass(midOut);
-		midOut = filter[1].lowpass(midOut);
+		outs[2] = filter[1].highpass(outs[1]);
+		outs[1] = filter[1].lowpass(outs[1]);
 
-		// Set individual outputs
-		outputs[LOW_OUTPUT].setVoltage(lowOut);
-		outputs[MID_OUTPUT].setVoltage(midOut);
-		outputs[HIGH_OUTPUT].setVoltage(highOut);
+		/////////////// Output
+		// Set gains and outs
 
-		// Reconstruct for main output
-		// This is pointless until the gain is implemented
-		float mainOut = lowOut + midOut + highOut;
+		float mainOut = 0.f;
+		for (int i = 0; i < 3; ++i)
+		{
+			outs[i] *= gains[i];
+			outputs[LOW_OUTPUT + i].setVoltage(outs[i]);
+			mainOut += outs[i];
+		}
+
 		outputs[OUT_OUTPUT].setVoltage(mainOut);
 	}
 };
