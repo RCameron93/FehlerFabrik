eb0ba143baebaf60f68ec58b124ff124be5cde71
Ross Cameron <rosscameron93@gmail.com>
2026-02-11 22:14:39 +0000

    selecting at least one random sequence

--- src/Shaney.cpp
+++ src/Shaney.cpp
@@ -1,14 +1,19 @@
 #include "plugin.hpp"
 
 #include "ffCommon.hpp"
+#include "random.hpp"
+#include <algorithm>
+#include <cstddef>
+#include <iterator>
 
 const int n_steps = 8;
+const int n_elements = n_steps * n_steps;
 // For a sequence of n steps the Markov array will have n*n elements
 
 struct Shaney : Module {
 	enum ParamId {
 		ENUMS(PROB_PARAM, n_steps),
-		ENUMS(STEP_PARAM, n_steps * n_steps),
+		ENUMS(STEP_PARAM, n_elements),
 		PARAMS_LEN
 	};
 	enum InputId {
@@ -29,13 +34,62 @@ struct Shaney : Module {
 	dsp::SchmittTrigger clockTrigger;
 	dsp::SchmittTrigger resetTrigger;
 
+	dsp::SchmittTrigger jumpTriggers[n_steps];
+
 	int sequencer_index = 0;
 
 	Shaney() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+
+        configInput(CLOCK_INPUT, "External Clock Trigger");
+
+		for (int i = 0; i < n_elements; ++i)
+		{
+			float default_value = (i % n_steps == i / n_steps) ? 1.f : 0.f;
+			std::string knob_name = string::f("Probability %d", i);
+			configParam(PROB_PARAM + i, 0.f, 1.f, default_value, knob_name);
+		}
+
+		for (int i = 0; i < n_steps; ++i)
+		{
+            configInput(JUMP_INPUT + i, string::f("Step %d Jump", i + 1));
+            configOutput(GATE_OUTPUT + i, string::f("Step %d Gate Output", i + 1));
+            configLight(STEP_LIGHT + i, string::f("Step %d", i + 1));
+		}
 	}
 
 	void process(const ProcessArgs& args) override {
+		if (inputs[CLOCK_INPUT].isConnected())
+		{
+			if (clockTrigger.process(inputs[CLOCK_INPUT].getVoltage()))
+			{
+
+				// Add an extra step for the probabilty of the sequence stopping
+				float cumulative_probabilities[n_steps + 1] = {0};
+				float running_total = 0.0;
+				for (int i = 0; i < n_steps; ++i)
+				{
+					float this_probability = params[PROB_PARAM + i + (n_steps * sequencer_index)].getValue();
+					running_total += this_probability;
+					cumulative_probabilities[i] = running_total;
+				}
+				float chance_of_stopping = fmax(0, 1 - running_total);
+				running_total += chance_of_stopping;
+				cumulative_probabilities[n_steps] = running_total;
+				float random_value = random::uniform() * running_total;
+				auto bisected = std::upper_bound (std::begin(cumulative_probabilities), std::end(cumulative_probabilities), random_value);
+				std::size_t new_index = bisected - std::begin(cumulative_probabilities);
+				outputs[GATE_OUTPUT + 1].setVoltage(running_total);
+				outputs[GATE_OUTPUT + 2].setVoltage(random_value);
+				outputs[GATE_OUTPUT + 3].setVoltage(chance_of_stopping);
+				outputs[GATE_OUTPUT].setVoltage(new_index);
+				// for (int i = 0; i < n_steps; ++i)
+				// {
+				// 	outputs[GATE_OUTPUT + i].setVoltage(cumulative_probabilities[i + 1]);
+				// }
+			}
+
+		}
 	}
 };
 
@@ -50,11 +104,14 @@ struct ShaneyWidget : ModuleWidget {
 		addChild(createWidget<FFHexScrew>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<FFHexScrew>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
+		float input_x_base = 160;
+		Vec clock_input_pos = Vec(input_x_base, 30);
+		addInput(createInputCentered<FF01JKPort>(mm2px(clock_input_pos), module, Shaney::CLOCK_INPUT));
+
 		float knob_x_base = 2 * RACK_GRID_WIDTH;
 		float knob_y_base = 24;
 		float x_delta = 13;
 		float y_delta = 11;
-
 
 		for (int i = 0; i < n_steps; ++i)
 		{
